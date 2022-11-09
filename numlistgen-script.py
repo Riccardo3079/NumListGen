a6a7298a17eee3632788e7402e09f489049e4b3b
Riccardo3079 <112393855+Riccardo3079@users.noreply.github.com>
2022-11-09 17:09:18 +0100

    Add files via upload
    Questo piccolo script genera una lista di numeri in ordine crescente fino ad un numero desiderato, senza bisogno di scriverli a mano, risparmiando tempo nel caso si voglia fare un elenco di numeri.

---
+++ numlistgen-script.py
@@ -0,0 +1,12 @@
+n = int(input('Inserire un numero: '))
+
+for i in range(n+1):
+        print(i)
+info = ("NumListGen v0.1   Riccardo3000   2022")
+
+comando_per_info = input('per maggiori informazioni, digitare "info".')
+
+if comando_per_info == "info":
+    print(info)
+        
+        
